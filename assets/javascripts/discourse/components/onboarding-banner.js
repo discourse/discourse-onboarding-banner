69275607a719805d551a307a091515564963172e
Jarek Radosz <jradosz@gmail.com>
2023-11-19 22:43:37 +0100

    DEV: Update linting setup (#17)

--- assets/javascripts/discourse/components/onboarding-banner.js
+++ assets/javascripts/discourse/components/onboarding-banner.js
@@ -1,9 +1,9 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
 import PostCooked from "discourse/widgets/post-cooked";
-import { action } from "@ember/object";
 
 export default class OnboardingBanner extends Component {
   @service router;
